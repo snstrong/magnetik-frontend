7b2b8bf934339d41995694091e85ff42e1d73ab9
Sara Strong <sara.n.strong@gmail.com>
2021-10-21 22:27:45 -0400

    Integrated react-konva drag and drop

--- src/writespace/WordTile.js
+++ src/writespace/WordTile.js
@@ -1,12 +1,36 @@
-function WordTile({ word, id }) {
+import { Layer, Text, Rect } from "react-konva";
+
+function WordTile({ word, id, x, y }) {
+  //   return (
+  //     <div
+  //       className="WordTile p-1 d-inline"
+  //       style={{ border: "1pt solid black" }}
+  //       id={id}
+  //     >
+  //       {word}
+  //     </div>
+  //   );
+
   return (
-    <div
-      className="WordTile p1"
-      style={{ border: "1pt solid black", width: "7rem" }}
-      id={id}
-    >
-      {word}
-    </div>
+    <Layer draggable id={id}>
+      <Rect
+        x={x}
+        y={y}
+        width={word.length * 12}
+        height={35}
+        stroke="#555"
+        fill="white"
+      />
+      <Text
+        x={x}
+        y={y}
+        text={word}
+        align="center"
+        padding={5}
+        fontSize={14}
+        width={word.length * 10}
+      />
+    </Layer>
   );
 }
 
--- src/writespace/Writespace.js
+++ src/writespace/Writespace.js
@@ -1,9 +1,22 @@
 import { useState, useEffect } from "react";
 import MagnetikApi from "../api/MagnetikApi";
-import WordDrawer from "./WordDrawer";
+import { Stage, Group, Layer, Text, Rect } from "react-konva";
+import { Portal } from "react-konva-utils";
+
+/** TODO:
+ * - Update state when x,y of word tile changes
+ * - Handlers for drag events (see https://codesandbox.io/s/github/konvajs/site/tree/master/react-demos/basic_demo?from-embed=&file=/src/index.js)
+ * - Saving to LocalStorage and/or db
+ * - Export as image
+ * - Separate layers for poem and unused word tiles with Portal
+ * - Improve initial spacing between tiles
+ * - Improve sizing for word tiles
+ * - Make size of Stage dynamic
+ */
 
 function Writespace() {
   const [wordList, setWordList] = useState(null);
+  const [wordTiles, setWordTiles] = useState(null);
 
   useEffect(function loadWordList() {
     console.debug("Writespace useEffect loadWordList");
@@ -11,20 +24,60 @@ function Writespace() {
       try {
         let wl = await MagnetikApi.getWordList();
         setWordList(wl);
+        let tiles = {};
+        for (let wordObj of wl) {
+          let { id, word } = wordObj;
+          tiles[id] = {
+            word: word,
+            x: Math.random() * window.innerWidth,
+            y: Math.random() * window.innerHeight,
+          };
+        }
+        setWordTiles({ ...tiles });
       } catch (err) {
-        console.error("Writespace useEffect fetchWordList");
+        console.error("Writespace useEffect fetchWordList", err);
       }
     }
     fetchWordList();
   }, []);
 
-  if (!wordList) return <h1>Loading...</h1>;
+  if (!wordList || !wordTiles) return <h1>Loading...</h1>;
 
   return (
-    <div className="container">
-      <div style={{ height: "400px", backgroundColor: "aliceblue" }}></div>
-      <WordDrawer wordList={wordList} />
-    </div>
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Rect
+          fill="#b6bbc2"
+          width={window.innerWidth}
+          height={window.innerHeight}
+        />
+        {wordList.map((wordObj) => {
+          return (
+            <Group
+              draggable
+              id={`${wordObj.id}`}
+              key={`${wordObj.id}`}
+              x={20}
+              y={Math.random() * (window.innerHeight - 10)}
+            >
+              <Rect
+                width={wordObj.word.length * 11}
+                height={35}
+                stroke="#555"
+                fill="white"
+              />
+              <Text
+                text={wordObj.word}
+                align="center"
+                padding={5}
+                fontSize={14}
+                fontFamily="monospace"
+              />
+            </Group>
+          );
+        })}
+      </Layer>
+    </Stage>
   );
 }
 
